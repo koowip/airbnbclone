0934c290e17464f0000e70c777f8428636ee269d
Patrick White <patrickwhite.swe@gmail.com>
2023-05-22 17:46:37 -0700

    Tailwind Configure
    Deleted next app tailwind config and made it baseline

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
 import { Nunito } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Airbnb',
@@ -20,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={font.className}>{children}</body>
     </html>
   )
 }
