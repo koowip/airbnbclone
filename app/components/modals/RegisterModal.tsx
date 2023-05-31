46a4dfdd75a0b5b4b02f19a1686704b9127b2f5c
Patrick White <patrickwhite.swe@gmail.com>
2023-05-31 16:49:19 -0700

    Sign up input box UI completed
    axios api not working yet but ui is looking good

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -5,10 +5,10 @@ import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
 import { useCallback, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
-
-
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal';
+import Heading from '../Heading';
+import Input from '../Inputs/Input';
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
@@ -40,6 +40,41 @@ const RegisterModal = () => {
                 setIsLoading(false);
             })
     }
+
+    const bodyContent = (
+        <div className='flex flex-col gap-4'>
+            <Heading 
+                title="Welcome to Airbnb"
+                subtitle='Create an Account!'
+                center
+            />
+            <Input 
+                id="email"
+                label="Email"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+            <Input 
+                id="name"
+                label="Name"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+            <Input 
+                id="password"
+                type='password'
+                label="Password"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+        </div>
+    )
     return (
         <Modal 
             disabled={isLoading}
@@ -48,6 +83,7 @@ const RegisterModal = () => {
             actionLabel='Continue'
             onClose={registerModal.onClose}
             onSubmit={handleSubmit(onSubmit)}
+            body={bodyContent}
         />
     );
 }
