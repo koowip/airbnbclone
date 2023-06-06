ad52278f3068e18502889dc0951900d198f03eb5
Patrick White <patrickwhite.swe@gmail.com>
2023-06-05 18:01:03 -0700

    Implementing Listing process
    Step 1, choosing category with form finished

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -9,6 +9,7 @@ import LoginModal from '../modals/LoginModal';
 import useLoginModal from '@/app/hooks/useLoginModal';
 import { signOut } from 'next-auth/react';
 import { SafeUser } from '@/app/types';
+import useRentModal from '@/app/hooks/useRentModal';
 
 interface UserMenuProps {
     currentUser?: SafeUser | null
@@ -18,17 +19,26 @@ const UserMenu: React.FC<UserMenuProps> = ({
 }) => {
     const registerModal = useRegisterModal();
     const loginModal = useLoginModal();
+    const rentModal = useRentModal();
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleOpen = useCallback(() => {
         setIsOpen((value) => !value);
     }, []);
 
+const onRent = useCallback(() => {
+    if(!currentUser) {
+      return loginModal.onOpen();
+    }
+
+    rentModal.onOpen();
+}, [currentUser, loginModal, rentModal]);
+
     return(
         <div className="relative">
             <div className="flex flex-row items-center gap-3">
                 <div
-                    onClick={() => {}} 
+                    onClick={onRent} 
                     className="
                         hidden
                         md:block
@@ -98,7 +108,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                                     label="My properties" 
                                 />
                                 <MenuItem
-                                    onClick={() => {}}
+                                    onClick={rentModal.onOpen}
                                     label="Airbnb my home" 
                                 />
                                 <MenuItem
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import RegisterModal from './components/modals/RegisterModal'
 import ToasterProvider from './Providers/ToasterProvider'
 import LoginModal from './components/modals/LoginModal'
 import getCurrentUser from './actions/getCurrentUser'
+import RentModal from './components/modals/RentModal'
 
 export const metadata = {
   title: 'Airbnb',
@@ -27,6 +28,7 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider/>
+          <RentModal />
           <LoginModal />
           <RegisterModal />
           <Navbar currentUser={currentUser}/>
